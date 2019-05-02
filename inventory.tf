420a2b5ba4594a9fd8772353191a7d5cb2c00325
Sahid Khan <sahidk@motabilityoperations.co.uk>
2019-05-02 12:04:24 +0100

    added code

---
+++ inventory.tf
@@ -0,0 +1,15 @@
+data "template_file" "inventory" {
+  template = "${file("${path.cwd}/helper_scripts/ansible-hosts.template.txt")}"
+  vars {
+    public_subdomain = "${aws_lb.infra_alb.dns_name}"
+    admin_hostname = "${aws_lb.master_alb.dns_name}"
+    master1_hostname = "${aws_instance.master1.private_dns}"
+    infra1_hostname = "${aws_instance.infra1.private_dns}"
+    worker1_hostname = "${aws_instance.worker1.private_dns}"
+    demo_htpasswd = "${var.htpasswd}"
+  }
+}
+resource "local_file" "inventory" {
+  content     = "${data.template_file.inventory.rendered}"
+  filename = "${path.cwd}/inventory/ansible-hosts"
+}
